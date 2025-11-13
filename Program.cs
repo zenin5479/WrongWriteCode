e50069624df6e32e691a5863c17e431ae36e29e9
zenin5479 <zenin5479@gmail.com>
2025-11-13 09:20:54 +0300

    Update 8.3 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -154,7 +154,7 @@ namespace WrongWriteCode
             }
          }
 
-         void AverageColumnsArray()
+         void AverageColumnArrayInt()
          {
             double average;
             Console.WriteLine("Среднеарифметическое столбцов:");
@@ -183,7 +183,7 @@ namespace WrongWriteCode
          Console.WriteLine("");
          PrintArrayInt();
          Console.WriteLine("");
-         AverageColumnsArray();
+         AverageColumnArrayInt();
 
          Console.WriteLine();
 
