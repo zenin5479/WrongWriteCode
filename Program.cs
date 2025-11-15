38a5077d41a19b33f65bab7270f5bd16f2bdaaa8
zenin5479 <zenin5479@gmail.com>
2025-11-15 11:00:58 +0300

    Update 17.4 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -298,26 +298,29 @@ namespace WrongWriteCode
          // Метод вывода массива
          void Print2DArray(int[,] c)
          {
-            while ()
+
+            int i = 0;
+            while (i < c.GetLength(0))
             {
-
-               while ()
+               int j = 0;
+               while (j < c.GetLength(1))
                {
-                  
-               }
 
-
-            }
-            
-            
-            for (int i = 0; i < c.GetLength(0); i++)
-            {
-               for (int j = 0; j < c.GetLength(1); j++)
-               {
                   Console.Write("{0} ", c[i, j]);
+
+                  j++;
                }
 
-               Console.WriteLine();
+               i++;Console.WriteLine();
+            }
+            for ( )
+            {
+               for ( )
+               {
+
+               }
+
+               
             }
          }
 
